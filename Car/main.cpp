c70eaafd6d360f2f8f436af64270001b3aaa5c0a
zheka <zheka@HOME-PC>
2025-04-22 20:36:09 +0300

    Engine_DONE

--- Car/main.cpp
+++ Car/main.cpp
@@ -58,17 +58,76 @@ public:
 		cout << "Fuel level:\t" << fuel_level<< " liters.\n";
 	}
 };
+#define MIN_ENGINE_CONSUMPTION 3
+#define MAX_ENGINE_CONSUMPTION 25
+class Engine
+{
+	const double CONSUMPTION;	//расход на 100км
+	const double DEFAULT_CONSUMPTION_PER_SECOND;
+	double consumption_per_second;
+	bool is_started;
+public:
+	double get_consumption_per_second()const
+	{
+		return consumption_per_second;
+	}
+	Engine(double consumption) :
+		CONSUMPTION
+		(
+			consumption < MIN_ENGINE_CONSUMPTION ? MIN_ENGINE_CONSUMPTION :
+			consumption > MAX_ENGINE_CONSUMPTION ? MAX_ENGINE_CONSUMPTION :
+			consumption
+		),
+		DEFAULT_CONSUMPTION_PER_SECOND(CONSUMPTION * 3e-5),
+		consumption_per_second(DEFAULT_CONSUMPTION_PER_SECOND)
+	{
+		cout << "Engine is ready" << endl;
+	}
+	~Engine()
+	{
+		cout << "Engine is over" << endl;
+	}
+	void start()
+	{
+		is_started = true;
+	}
+	void stop()
+	{
+		is_started = false;
+	}
+	bool started()const
+	{
+		return is_started;
+	}
+	void info()
+	{
+		cout<<"ConsumptionL "<< CONSUMPTION <<"liters/100km\n"<< endl;
+		cout<<"Default Consumption "<< DEFAULT_CONSUMPTION_PER_SECOND <<"liters/s\n"<< endl;
+		cout<<"Consumption "<< consumption_per_second <<"liters/s\n"<< endl;
+	}
+};
+//#define TANK_CHECK
+//#define ENGINE_CHECK
 
 void main()
 {
 	setlocale(LC_ALL, "RU");
+#ifdef TANK_CHEK
 	Tank tank(80);
 	double fuel;
 	do
 	{
-		cout << "На сколько заправляемся? ";cin>> fuel;
+		cout << "На сколько заправляемся? "; cin >> fuel;
 		tank.fill(fuel);
 		tank.info();
 	} while (true);
 	tank.info();
+#endif // TANK_CHEK
+#ifdef ENGINE_CHECK
+
+	Engine engine(10);
+	engine.info();
+
+#endif // ENGINE_CHECK
+
 }
